f295cc4ef5b9e45af64a10357ffc0d6736b51f9b
Ayat Aliev <ayataliev0@gmail.com>
2020-02-09 17:40:58 +0600

    l

--- app/src/main/java/com/geektech/film/ui/main/recyclerview/Adapter.java
+++ app/src/main/java/com/geektech/film/ui/main/recyclerview/Adapter.java
@@ -28,7 +28,7 @@ public class Adapter extends RecyclerView.Adapter<ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main,parent,false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
         return new ViewHolder(v);
     }
 
